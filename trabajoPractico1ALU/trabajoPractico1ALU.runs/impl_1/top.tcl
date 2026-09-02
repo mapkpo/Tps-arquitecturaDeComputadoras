4f6c915883f21949ada380d2abe672dcf4fc3845
Mijail Potinski <128333142+mapkpo@users.noreply.github.com>
2026-09-02 13:24:44 -0300

    Add files via upload

--- trabajoPractico1ALU/trabajoPractico1ALU.runs/impl_1/top.tcl
+++ trabajoPractico1ALU/trabajoPractico1ALU.runs/impl_1/top.tcl
@@ -97,6 +97,8 @@ proc step_failed { step } {
 OPTRACE "impl_1" END { }
 }
 
+set_msg_config -id {Synth 8-256} -limit 10000
+set_msg_config -id {Synth 8-638} -limit 10000
 
 OPTRACE "impl_1" START { ROLLUP_1 }
 OPTRACE "Phase: Init Design" START { ROLLUP_AUTO }
@@ -104,8 +106,11 @@ start_step init_design
 set ACTIVE_STEP init_design
 set rc [catch {
   create_msg_db init_design.pb
+  set_param checkpoint.writeSynthRtdsInDcp 1
   set_param general.usePosixSpawnForFork 1
   set_param chipscope.maxJobs 3
+  set_param synth.incrementalSynthesisCache C:/Users/Misha/AppData/Roaming/Xilinx/Vivado/.Xil/Vivado-684-DESKTOP-POAFI82/incrSyn
+  set_param xicom.use_bs_reader 1
   set_param runs.launchOptions { -jobs 12  }
 OPTRACE "create in-memory project" START { }
   create_project -in_memory -part xc7a35tcpg236-1
--- trabajoPractico1ALU/trabajoPractico1ALU.runs/synth_1/top.tcl
+++ trabajoPractico1ALU/trabajoPractico1ALU.runs/synth_1/top.tcl
@@ -56,7 +56,13 @@ if {$::dispatch::connected} {
 }
 
 OPTRACE "synth_1" START { ROLLUP_AUTO }
+set_param checkpoint.writeSynthRtdsInDcp 1
 set_param general.usePosixSpawnForFork 1
+set_param chipscope.maxJobs 3
+set_param synth.incrementalSynthesisCache C:/Users/Misha/AppData/Roaming/Xilinx/Vivado/.Xil/Vivado-684-DESKTOP-POAFI82/incrSyn
+set_param xicom.use_bs_reader 1
+set_msg_config -id {Synth 8-256} -limit 10000
+set_msg_config -id {Synth 8-638} -limit 10000
 OPTRACE "Creating in-memory project" START { }
 create_project -in_memory -part xc7a35tcpg236-1
 
